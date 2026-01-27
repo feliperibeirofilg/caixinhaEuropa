9fd7558b5862686a6c3ab59bcd45cbb082f22da6
feliperibeirofilg <felipearaujorib@icloud.com>
2026-01-27 12:34:55 -0300

    Ajustando estilização

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Europa Ai Vou eu</title>
+    <title>BoardingBox</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     
@@ -21,8 +21,8 @@
         <nav class="custom-navbar">
             {{-- Lado Esquerdo: Logo --}}
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('icon/icon.png') }}" alt="Logo" height="35">
-                <span>Europa Aí Vou Eu</span>
+                <img src="{{ asset('icon/icone.avif') }}" alt="Logo" height="35">
+                <span>BoardingBox</span>
             </a>
 
             {{-- Lado Direito: Menu --}}
